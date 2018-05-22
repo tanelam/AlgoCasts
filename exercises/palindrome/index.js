5616e2f1b2a8dc24dd04de8a0c8d0e93794fb99c
tanelam <tania.e.aparicio@gmail.com>
2018-05-22 13:02:00 -0400

    done

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,38 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+//My solution
+
+function palindrome(str) {
+  let newStr = ""
+
+  for(let i=0; i < str.length; i++){
+    newStr = str[i] + newStr
+  }
+
+  return newStr === str ? true : false
+
+}
+
+//Solution 1
+
+// function palindrome(str){
+//
+//   let reversed = str.split("").reverse().join("")
+//
+//   return reversed === str
+// }
+
+//Solution 2 uses the helper method "every"
+
+// function palindrome(str){
+//   return str.split("").every((char, i) => {
+//     return char === str[str.length - i - 1]
+//   })
+// }
+
+
+
+
 
 module.exports = palindrome;
