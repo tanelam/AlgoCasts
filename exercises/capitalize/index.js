05fa70b9942eb6006fa76c66709e335134bc2d50
tanelam <tania.e.aparicio@gmail.com>
2018-05-23 15:15:27 -0400

    done

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,7 +6,28 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+//
+function capitalize(str) {
+  const strToArr = str.split(" ")
+  const words = []
 
-function capitalize(str) {}
+  for(let i = 0; i < strToArr.length; i++){
+    words.push(strToArr[i][0].toUpperCase() + strToArr[i].slice(1))
+  }
+
+  return words.join(" ")
+}
 
 module.exports = capitalize;
+
+//Solution using a different type of for loop
+// function capitalize(str){
+//   let words = []
+//   let array = str.split(" ")
+//
+//   for(let word of array){
+//     words.push(word[0].toUpperCase() + word.slice(1))
+//   }
+//
+//   return words.join(" ")
+// }
