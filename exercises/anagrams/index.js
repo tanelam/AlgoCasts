3d1ddaa80af61220b99df0d33d7e0bd1a77f518e
tanelam <tania.e.aparicio@gmail.com>
2018-05-23 14:56:12 -0400

    done

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,7 +8,7 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-// Refactored Solution
+// Refactored Solution with helper method
 function anagrams(stringA, stringB){
   const strA = sortedArr(stringA)
   const strB = sortedArr(stringB)
@@ -27,6 +27,41 @@ function sortedArr(str){
 //
 //   return sortedA === sortedB
 // }
+
+// Solution using character map, iteration and a helper method
+// function anagrams(stringA, stringB) {
+//
+// let regStrA = stringA.replace(/[^\w]/g, "").toLowerCase()
+// let regStrB = stringB.replace(/[^\w]/g, "").toLowerCase()
+//
+// let charMapA = buildCharMap(regStrA)
+// let charMapB = buildCharMap(regStrB)
+//
+//   if(regStrA.length !== regStrB.length){
+//     return false
+//   }
+//
+//   for(let key in charMapA){
+//     if(charMapA[key] !== charMapB[key]){
+//       return false
+//     }
+//   }
+//   return true
+// }
+//
+// function buildCharMap(str){
+//   let obj = {}
+//
+//   for(let char of str){
+//     if(obj[char]){
+//       obj[char]++
+//     }else{
+//       obj[char] = 1
+//     }
+//   }
+//   return obj
+// }
+
 
 
 module.exports = anagrams;
